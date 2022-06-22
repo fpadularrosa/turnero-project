87d97a92daa1b54aaa725ac14fa9e2f0422a4feb
Braian Serra <bnserra@gmail.com>
2022-06-22 19:46:01 -0300

    Boilerplate finished

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,11 +1,4 @@
-import { sortAsc, sortDesc } from "../../utils/sortFunctions"
-import { GET_CATEGORIES, GET_ITEMS, ORDER_ITEMS, RESET_FILTERS, SEARCH_ITEMS, USER_LOGIN, USER_LOGOUT } from "../actions"
-
 const initialState = {
-    rawItems: [],
-    items: [],
-    userLogged: null,
-    categories: [],
 }
 
 export default function rootReducer(state = initialState, action){
